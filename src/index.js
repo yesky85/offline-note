68be8b107b05e9ebe9c1f9734625b97161f74d4e
kec <57247368+yesky85@users.noreply.github.com>
2020-05-15 16:42:44 +0900

    Initial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { ApolloProvider } from "react-apollo";
 import GlobalStyles from "./components/globalStyles";
+import client from "./apollo";
 
 ReactDOM.render(
-  <>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <App />
-  </>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
